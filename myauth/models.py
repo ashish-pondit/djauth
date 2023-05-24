9850665813864bded59173dcc95ed33c915fe149
Ashish Pondit <ashish.pondit@dsinnovators.com>
2023-05-24 17:30:25 +0600

    Change Base user model

--- myauth/models.py
+++ myauth/models.py
@@ -1,7 +1,74 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
-# Create your models here.
-class User(AbstractUser):
-    pass
+class MyUserManager(BaseUserManager):
+    def create_user(self, name, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name, email, password=None):
+        user = self.create_user(
+            name,
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    GENDER_CHOICES = [
+        ("M", "Male"),
+        ("F", "Female"),
+    ]
+
+    USER_TYPE_CHOICES = [
+        ("TO", "Tourist"),
+        ("GU", "Guide"),
+        ("AG", "Agency"),
+        ("AD", "Admin"),
+    ]
+
+    email = models.EmailField(
+        verbose_name="Email address",
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_image = models.FileField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    address = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    user_type = models.CharField(max_length=2, choices=USER_TYPE_CHOICES)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    object = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
